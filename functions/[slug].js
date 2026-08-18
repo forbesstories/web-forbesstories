fa9ad629192d829b9a8eee7796aadae2a43d24aa
forbesstories <forbesstories@gmail.com>
2026-08-18 13:16:16 +0700

    Update [slug].js

--- functions/[slug].js
+++ functions/[slug].js
@@ -76,6 +76,7 @@ ${cardImage(ogImage(p.slug),p.title)}
 				return layout({
 					title:post.title,
 					description:desc,
+					category:sanitizeSlug(post.kategori),
 					canonical:url,
 					image:og,
 					schema:seo({
